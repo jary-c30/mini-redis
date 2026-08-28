30526f369847525e3c539726044d672a03ec9868
Jary Chen <jarychen30@gmail.com>
2026-08-28 00:08:35 -0400

    finished the handle request

--- miniredis/protocol.py
+++ miniredis/protocol.py
@@ -60,7 +60,45 @@ def handle_integer(self, socket_file):
     line_string = socket_file.readline()
     decoded_string = line_string.decode('utf-8').rstrip('\r\n')
 
-    #wrapping decoded string integer so it turns into a rela number for example "1234" --> 1234
+    #wrapping decoded string integer so it turns into a real number for example "1234" --> 1234
     return int(decoded_string)
+
+
+def handle_string(self, socket_file):
+    #checking for the length of the string 
+    length_line = socket_file.readline()
+    length = int(length_line.decode('utf-8').rstrip('\r\n'))
+
+    #ccheking if the length of the string is -1 if so we return none as -1 means no value
+    if length == -1:
+        return None
+
+    #reads the actual value and returns the string with the last two character sliced off which are \r\n
+    return (socket_file.read(length + 2))[:-2]
+
+def handle_array(self, socket_file):
+    #checking for the length of the array
+    length_el_line = socket_file.readline()
+    length_el = int(length_el_line.decode('utf-8').rstrip('\r\n'))
+
+    # array has mulitple data types each element containing its own byte we can call the handle request to parse each of them
+    #this would also handle nested arrays as well since handle_request would call handle_array or handle_dict again
+    return [self.handle_request(socket_file) for _ in range(length_el)]
+
+def handle_dict(self, socket_file):
+    #checking for the length of the array
+    length_el_line = socket_file.readline()
+    length_el = int(length_el_line.decode('utf-8').rstrip('\r\n'))
+
+    #since there is 1 key paired with 1 value it means we need to read twice as many elements
+    length_total = length_el * 2
+
+    # since the dict is written as (key, value, key, value,.....) each with its own prefic byte, we can reuse 
+    #handl_request to parse each on recursivly
+    elements =  [self.handle_request(socket_file) for _ in range(length_total)]
+
+    #zip helps pair up every even index which is the key with every odd-index which is the value
+    #while dic constructs the final dict
+    return dict(zip(elements[::2], elements[1::2]))
 
 
